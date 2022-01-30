f808579c07b99e98b837457d936d9f2c80294b78
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-01-30 18:02:18 +0530

    Navbar, Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <h1>Madushan Nipuna</h1>
-    </div>
+
+    <BrowserRouter>
+
+    <Navbar/>
+    <main>
+      <Container>
+        <h1 align='center'>Welcome to My portfolio</h1>
+      </Container>
+    </main>
+
+    <Footer/>
+
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import './Footer.css'
+
+function Footer() {
+  return (
+      <>
+        <footer>
+            <Container>
+                <Row>
+                    <h1 align='center'>HI</h1>
+                </Row>
+            </Container>
+        </footer>
+      </>
+      
+  )}
+
+export default Footer;
